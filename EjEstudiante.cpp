9dd691dfd302e699a977e49518f3c2c39fe9a6ee
FideoMendez <159058855+FideoMendez@users.noreply.github.com>
2024-03-04 13:54:44 -0400

    Update and rename class_Estudiante.cpp to EjEstudiante.cpp

--- EjEstudiante.cpp
+++ EjEstudiante.cpp
@@ -4,26 +4,6 @@
 
 using namespace std;
 
-Estudiante::Estudiante() {
-
-}
-Estudiante::~Estudiante() {
-
-}
-void Estudiante::set_nombre(string _n) {
-	//Da valores al atributo nombre de la clase
-	nombre = _n;
-}
-string Estudiante::get_nombre() {
-	return nombre;
-}
-void Estudiante::set_edad(int _e) {
-	edad = _e;
-}
-int Estudiante::get_edad() {
-	return edad;
-}
-
 int main()
 {
     //Declarar un objeto de la clase estudiante
